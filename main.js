2a8ec01a35f269e1c0b0c3541651131f566093c0
Alafia-Ojo <ojoalafiadavid@gmail.com>
2022-12-15 19:00:31 +0100

    update

--- main.js
+++ main.js
@@ -162,5 +162,3 @@ const observer = new IntersectionObserver((entries) => {
   });
 });
 
-const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((el) => observer.observe(el))
